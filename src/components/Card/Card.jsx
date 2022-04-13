6a6175e7cc0263c409a600d37689468d3f4f148a
Francisco Rappazzini <fran_rappa@outlook.com>
2022-04-13 16:22:24 -0300

    cards hover

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,42 @@
 import "./Card.scss";
 
+import React, { useState } from "react";
+
 import BtnCustom from "../BtnCustom/BtnCustom";
-import React from "react";
 
 function Card({ img, nombre, descripcion }) {
+  const [click, setClick] = useState(false);
+
+  function handleClick() {
+    setClick(!click);
+  }
+
+  function addClass() {
+    return click ? "class__active" : "";
+  }
+
   return (
     <article
-      className="proyecto__card ih-item square effect13 left_to_right img"
+      className={`proyecto__card ih-item square effect13 left_to_right img ${addClass()}`}
       style={{ backgroundImage: `url(${img})` }}
     >
-      <a href="">
-        <p className="nombre_text m-0">{nombre}</p>
+      {!click && <p className="nombre_text m-0">{nombre}</p>}
 
-        {window.innerWidth < 835 && (
-          <div className="d-flex justify-content-end">
-            <BtnCustom text={"+ info"} />
-          </div>
-        )}
-
-        {/* effect */}
-
-        <div class="info">
-          <h3>Heading here</h3>
-          <p>Description goes here</p>
+      {window.innerWidth < 835 && !click && (
+        <div className="d-flex justify-content-end">
+          <BtnCustom text={"Saber +"} onClick={handleClick} />
         </div>
-      </a>
+      )}
+
+      {/* hover */}
+      <div class="info d-flex flex-column justify-content-between">
+        <h3>{nombre}</h3>
+        <p>{descripcion}</p>
+
+        <div className="d-flex justify-content-end">
+          <BtnCustom text={"+ info"} />
+        </div>
+      </div>
     </article>
   );
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ import { categorias } from "../../data/portfolioData";
 function Portfolio() {
   return (
     <section className="container-fluid portfolio__container">
-      <h3 className="text-center">Portfolio</h3>
+      <h3 className="text-center mb-5">Portfolio</h3>
 
       <section className="cards__container">
         {categorias.map((cat) => (

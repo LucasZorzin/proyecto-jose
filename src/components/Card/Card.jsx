0a8e3d94d70bc91cbe5d8da4e321713a4e1d9737
Francisco Rappazzini <fran_rappa@outlook.com>
2022-04-13 13:35:26 -0300

    fonts + hover effect added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,12 +6,25 @@ import React from "react";
 function Card({ img, nombre, descripcion }) {
   return (
     <article
-      className="proyecto__card"
+      className="proyecto__card ih-item square effect13 left_to_right img"
       style={{ backgroundImage: `url(${img})` }}
     >
-      <p className="nombre_text m-0">{nombre}</p>
+      <a href="">
+        <p className="nombre_text m-0">{nombre}</p>
 
-      <BtnCustom text={"+ info"} />
+        {window.innerWidth < 835 && (
+          <div className="d-flex justify-content-end">
+            <BtnCustom text={"+ info"} />
+          </div>
+        )}
+
+        {/* effect */}
+
+        <div class="info">
+          <h3>Heading here</h3>
+          <p>Description goes here</p>
+        </div>
+      </a>
     </article>
   );
 }

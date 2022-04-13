1d2a5c60834d95c496a8954cd11c84957740aca0
Francisco Rappazzini <fran_rappa@outlook.com>
2022-04-13 16:55:41 -0300

    section ayuda

--- src/components/BtnCustom/BtnCustom.jsx
+++ src/components/BtnCustom/BtnCustom.jsx
@@ -2,9 +2,13 @@ import "./BtnCustom.scss";
 
 import React from "react";
 
-function BtnCustom({ text, onClick }) {
+function BtnCustom({ text, onClick, fontSize }) {
   return (
-    <button className="btn__custom" onClick={onClick}>
+    <button
+      className="btn__custom"
+      onClick={onClick}
+      style={{ fontSize: fontSize ? fontSize : "auto" }}
+    >
       {text}
     </button>
   );
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,18 +1,37 @@
 import "./Portfolio.scss";
 
+import BtnCustom from "../BtnCustom/BtnCustom";
 import Card from "../Card/Card";
 import React from "react";
 import { categorias } from "../../data/portfolioData";
 
 function Portfolio() {
+  function btnSize() {
+    const innerWidth = window.innerWidth;
+
+    if (innerWidth >= 415) {
+      return 36;
+    } else {
+      return 18;
+    }
+  }
+
   return (
     <section className="container-fluid portfolio__container">
-      <h3 className="text-center mb-5">Portfolio</h3>
+      <h3 className="text-center mb-4 portfolio__text">Portfolio</h3>
 
       <section className="cards__container">
         {categorias.map((cat) => (
           <Card key={cat.id} {...cat} />
         ))}
+      </section>
+
+      <section className="ayuda__container d-flex flex-column align-items-center">
+        <p>¿Necesitas ayuda con tus textos?</p>
+
+        <div className="img__container d-flex justify-content-center align-items-center mb-5">
+          <BtnCustom text={"Escríbeme"} fontSize={btnSize()} />
+        </div>
       </section>
     </section>
   );

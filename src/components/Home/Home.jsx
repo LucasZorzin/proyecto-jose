e70cbc6a3bab48641921803184be4589852abf51
Lucas Zorzin <lucaszorzincml@gmail.com>
2022-04-17 23:36:18 -0300

    home section added (desktop+tablet+mobile version) + responsive bug fixes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,24 +8,29 @@ import vector from '../../assets/svg/vector.svg';
 function Home() {
     return (
         <>
-            <div className="home-header"><p>De los poetas aprendí que las palabras son poderosas, entonces decidí hacer de las letras mis mejores armas.</p></div>
+            <div className="layer-header">
+                <div className="home-header">
+                    <p>De los poetas aprendí que las palabras son poderosas, entonces decidí hacer de las letras mis mejores armas.</p>
+                </div>
+            </div>
             <div className="row sobre-mi">
-                <div className="col-7 sobre-mi__text">
+                <div className="col-12 col-md-7 sobre-mi__text">
                     <h3>Algo sobre mí</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
 
-                <div className="col-5 sobre-mi__vector">
-                    <img src={notebookVector} alt="vector" />
+                <div className="col col-md-5 sobre-mi__vector">
+                    <img className="notebookVector" src={notebookVector} alt="vector" />
                     <img className="notebook" src={notebook} alt="notebook" />
                 </div>
             </div>
             <img className="vector" src={vector} alt="vector" />
             <div className="row i-copywriting">
-                <div className="col-4 i-copywriting__title">
-                    <h3>La importancia del <br/><span>copywriting</span></h3>
+                <div className="col-12 col-md-6 col-lg-5 i-copywriting__title">
+                    <h3 className="i-copywriting-responsiveMP">La importancia del <br /><span>copywriting</span></h3>
+                    <h3 className="i-copywriting-responsiveT">La importancia <br/>del <span>copywriting</span></h3>
                 </div>
-                <div className="col-8 i-copywriting__text">
+                <div className="col-12 col-md-6 col-lg-7 i-copywriting__text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>

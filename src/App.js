42b66de78237bcd1a1357fc88ec4047699809840
Francisco Rappazzini <fran_rappa@outlook.com>
2022-04-19 00:11:50 -0300

    react router installed + screens created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.scss";
 
-import Navbar from "./components/NavBar/Navbar";
-import Home from "./components/Home/Home";
-import Portfolio from "./components/Portfolio/Portfolio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import Navbar from "./components/NavBar/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Portfolio />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/:id" element={<HomeScreen />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import Home from "../components/Home/Home";
+import Portfolio from "../components/Portfolio/Portfolio";
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <>
+      <Home />
+      <Portfolio />
+    </>
+  );
+}
+
+export default HomeScreen;
